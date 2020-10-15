28422937ef6c6570767cba3a76e3c79601e42e1f
Kev Marleau <marleauk1@newpaltz.edu>
2020-10-16 02:21:19 +0900

    reformatted burger code for readability

--- client/src/components/Nav.vue
+++ client/src/components/Nav.vue
@@ -1,69 +1,70 @@
 <template>
   <nav class="navbar is-info" role="navigation" aria-label="main navigation">
-  <div class="navbar-brand">
-    <router-link to="/" class="navbar-item">
-      <img src="../assets/logo.png" height="28">
-    </router-link>
+    <div class="navbar-brand">
+      <router-link to="/" class="navbar-item">
+        <img src="../assets/logo.png" height="28" />
+      </router-link>
 
-    <a role="button" class="navbar-burger burger" :class="{ 'is-active': isActive }" @click="isActive = !isActive" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
-      <span aria-hidden="true"></span>
-      <span aria-hidden="true"></span>
-      <span aria-hidden="true"></span>
-    </a>
-  </div>
+      <a
+        role="button"
+        class="navbar-burger burger"
+        :class="{ 'is-active': isActive }"
+        @click="isActive = !isActive"
+        aria-label="menu"
+        aria-expanded="false"
+        data-target="navbarBasicExample"
+      >
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+      </a>
+    </div>
 
-  <div id="navbarBasicExample" class="navbar-menu" :class="{ 'is-active': isActive }" @click="isActive = !isActive">
-    <div class="navbar-start">
+    <div
+      id="navbarBasicExample"
+      class="navbar-menu"
+      :class="{ 'is-active': isActive }"
+      @click="isActive = !isActive"
+    >
+      <div class="navbar-start">
+        <router-link to="/" class="navbar-item">Home</router-link>
+        <router-link to="/about" class="navbar-item">About</router-link>
+        <router-link to="/feed" class="navbar-item">Feed</router-link>
 
-      <router-link to="/" class="navbar-item">Home</router-link>
-      <router-link to="/about" class="navbar-item">About</router-link>
-      <router-link to="/feed" class="navbar-item">Feed</router-link>
+        <div class="navbar-item has-dropdown is-hoverable">
+          <a class="navbar-link"> More </a>
 
-      <div class="navbar-item has-dropdown is-hoverable">
-        <a class="navbar-link">
-          More
-        </a>
+          <div class="navbar-dropdown">
+            <a class="navbar-item"> Jobs </a>
+            <a class="navbar-item"> Contact </a>
+            <hr class="navbar-divider" />
+            <a class="navbar-item"> Report an issue </a>
+          </div>
+        </div>
+      </div>
 
-        <div class="navbar-dropdown">
-          <a class="navbar-item">
-            Jobs
-          </a>
-          <a class="navbar-item">
-            Contact
-          </a>
-          <hr class="navbar-divider">
-          <a class="navbar-item">
-            Report an issue
-          </a>
+      <div class="navbar-end">
+        <div class="navbar-item">
+          <LoginBadge />
         </div>
       </div>
     </div>
-
-    <div class="navbar-end">
-      <div class="navbar-item">
-        <LoginBadge />
-      </div>
-    </div>
-  </div>
-</nav>
+  </nav>
 </template>
 
 <script>
 import LoginBadge from "@/components/LoginBadge";
 
 export default {
-  data: ()=> ({
-    isActive: false
+  data: () => ({
+    isActive: false,
   }),
-  methods: {
-
-  },
+  methods: {},
   components: {
-    LoginBadge
-  }
-}
+    LoginBadge,
+  },
+};
 </script>
 
 <style>
-
 </style>
