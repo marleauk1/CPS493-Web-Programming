85294fd9c3f3c2eddb0a7750e4c5cc71b43a6778
Kev Marleau <marleauk1@newpaltz.edu>
2020-11-05 03:16:38 +0900

    async calls

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -4,14 +4,19 @@ const users = require('../models/users')
 const router = express.Router();
 
 router.get('/', (req, res) => {
-        throw { status: 501, message: "This is a fake error" }
-        res.send( users.getAll() );
+        users.getAll()
+        .then(x => res.send( x ) )
+        .catch(next);
     })
     .get('/search', (req, res) => {
-        res.send( users.search(req.query.q) );
+        res.send( users.search(req.query.q) )
+        .then(x => res.send( x ) )
+        .catch(next);
     })
     .post('/', (req, res) => {
-        const newUser = users.add( req.query.name, req.query.age );
+        const newUser = users.add( req.query.name, req.query.age )
+        .then(newUser => { res.send( newUser ); })
+        .catch(next);
         res.send( newUser );
     })
 
--- server/models/users.js
+++ server/models/users.js
@@ -1,11 +1,14 @@
 const data = [{ name: 'Moshe', age: 43}, { name: 'Joe', age: 78 }]
 
 function getAll(){
+    throw { status: 501, message: "This is a fake error" }
+    // await Promise.resolve()
+    console.log("Called getAll")
     return data;
 }
 
-function add(name, age){
+async function add(name, age){
     data.push({ name, age });
 }
 
-module.exports = { getAll, add, search: q => data.filter(x => x.name == q) }
+module.exports = { getAll, add, search: async q => data.filter(x => x.name == q) }
