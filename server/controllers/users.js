dac238a5044f9ce9fc8f39856d8453a567bb661f
Kev Marleau <marleauk1@newpaltz.edu>
2020-11-24 06:18:48 +0900

    sql select queries

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -3,17 +3,30 @@ const users = require('../models/users')
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
+router
+    .get('/', (req, res, next) => {
         users.getAll()
         .then(x => res.send( x ) )
         .catch(next);
     })
-    .get('/search', (req, res) => {
-        res.send( users.search(req.query.q) )
+    .get('/:id', (req, res, next) => {
+        const id = +req.params.id;
+        if(!id) return next();
+        users.get(id)
         .then(x => res.send( x ) )
         .catch(next);
     })
-    .post('/', (req, res) => {
+    .get('/types', (req, res, next) => {
+        users.getTypes()
+        .then(x => res.send( x ) )
+        .catch(next);
+    })
+    .get('/search', (req, res, next) => {
+        users.search(req.query.q)
+        .then(x => res.send( x ) )
+        .catch(next);
+    })
+    .post('/', (req, res, next) => {
         const newUser = users.add( req.query.name, req.query.age )
         .then(newUser => { res.send( newUser ); })
         .catch(next);
--- server/models/users.js
+++ server/models/users.js
@@ -1,14 +1,27 @@
+const mysql = require('./mysql');
+
 const data = [{ name: 'Moshe', age: 43}, { name: 'Joe', age: 78 }]
 
-function getAll(){
-    throw { status: 501, message: "This is a fake error" }
+async function getAll(){
+    // throw { status: 501, message: "This is a fake error" }
     // await Promise.resolve()
     console.log("Called getAll")
-    return data;
+    return await mysql.query(`SELECT * FROM Users`);
 }
+
+async function get(id){
+    return await mysql.query(`SELECT * FROM Users WHERE id=?`, [id]);
+}
+
+async function getTypes(){
+    return await mysql.query(`SELECT id, Name FROM Types WHERE Type_id = 2`);
+}
+
+const search = async q => await mysql.query(`SELECT id, FirstName, LastName FROM Users WHERE 
+    LastName LIKE ? OR FirstName LIKE ?; `, [`%${q}%`, `%${q}%`])
 
 async function add(name, age){
     data.push({ name, age });
 }
 
-module.exports = { getAll, add, search: async q => data.filter(x => x.name == q) }
+module.exports = { getAll, get, getTypes, search, add }
